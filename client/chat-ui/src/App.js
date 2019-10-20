acb6d566293eef7da417832b2595db2c20c69842
Gabriel Cabral <gabrielcabral@MacBook-Air.local>
2019-10-19 22:06:12 -0300

    merge feature/websocket-communication

--- client/chat-ui/src/App.js
+++ client/chat-ui/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Ws from '@adonisjs/websocket-client'
+const ws = Ws('ws://localhost:3000')
+ws.connect()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const chat = ws.subscribe('chat')
+
+chat.on('ready', () => {
+  chat.emit('message', 'hello')
+})
+
+chat.on('error', (error) => {
+  console.log(error)
+})
+
+chat.on('close', () => {
+})
+
+ws.getSubscription('chat').on('message', (message) => {
+  console.log("NEW MESSAGE: ", message);
+  
+})
+
+class App extends Component {
+
+  handleClick() {
+    chat.emit('message', 'new messsage!')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Realtime chat</p>
+        <button onClick={this.handleClick}>Enviar nova mensagem</button>
+      </div>
+    );
+  }
 }
 
 export default App;
